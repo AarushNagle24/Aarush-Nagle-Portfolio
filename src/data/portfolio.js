5dc6751183e43d6def8b1c506b31c30c863a10d5
Aarush Nagle <aarushnagle24@gmail.com>
2026-08-24 13:55:54 -0400

    Match Tower thumbnail to abstract project style

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -88,8 +88,8 @@ export const projects = [
     title: 'Tower of Easy',
     category: 'Roblox Game',
     date: 'Live March 2021 - Present',
-    image: asset('images/tower-of-easy.png'),
-    fallbackImage: asset('images/tower-of-easy.png'),
+    image: asset('images/tower-of-easy.svg'),
+    fallbackImage: asset('images/tower-of-easy.svg'),
     alt: 'Tower of Easy Roblox game thumbnail',
     live: 'https://www.roblox.com/games/6496227852/Tower-of-Easy',
     features: [
